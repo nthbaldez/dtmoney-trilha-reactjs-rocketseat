f3578c0ef056066f973ea0dd699a95b63af2e4b1
Nathan Baldez <nathanbsb2@hotmail.com>
2021-12-14 23:03:53 -0300

    :fire: Second commit :fire:

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+
+
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => console.log(response.status === 200 ? 'Deu bom' : 'Deu ruim'))
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>14/12/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$1.100</td>
+            <td>Desenvolvimento</td>
+            <td>14/12/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
